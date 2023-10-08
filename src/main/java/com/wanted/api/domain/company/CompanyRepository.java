cf7412cac7fe0c6a1488af2d02cad52ac74288f5
myoungjinseo <sih5673@gmail.com>
2023-10-08 22:07:53 +0900

    feat : 회사 레포지토리

---
+++ src/main/java/com/wanted/api/domain/company/CompanyRepository.java
@@ -0,0 +1,11 @@
+package com.wanted.api.domain.company;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CompanyRepository extends JpaRepository<Company,Long> {
+
+
+
+}
