361d98c8e6eb8db0bf1a7b127db1b727a4b277d4
myoungjinseo <sih5673@gmail.com>
2023-10-11 03:30:52 +0900

    feat : getId

---
+++ src/main/java/com/wanted/api/web/company/service/CompanyService.java
@@ -0,0 +1,21 @@
+package com.wanted.api.web.company.service;
+
+import com.wanted.api.common.exception.ErrorCode;
+import com.wanted.api.common.exception.ErrorException;
+import com.wanted.api.domain.company.Company;
+import com.wanted.api.domain.company.CompanyRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class CompanyService {
+    private final CompanyRepository companyRepository;
+
+    @Transactional(readOnly = true)
+    public Company getId(Long companyId){
+        return  companyRepository.findById(companyId)
+                .orElseThrow(() -> new ErrorException(ErrorCode.NOT_FOUND_COMPANY));
+    }
+}
