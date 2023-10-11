7f645a8e043635b7b4791e806c6b39d0ebbfff2d
myoungjinseo <sih5673@gmail.com>
2023-10-11 15:55:09 +0900

    feat : querydsl

--- src/main/java/com/wanted/api/domain/member/MemberEmploymentRepository.java
+++ src/main/java/com/wanted/api/domain/member/MemberEmploymentRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MemberEmploymentRepository extends JpaRepository<MemberEmployment,Long> {
+public interface MemberEmploymentRepository extends JpaRepository<MemberEmployment,Long> ,MemberEmploymentRepositoryCustom {
 
 
 
