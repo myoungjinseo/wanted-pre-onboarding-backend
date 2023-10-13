b35e76b70735210a9260de5d8db227113156f3b6
myoungjinseo <sih5673@gmail.com>
2023-10-13 13:49:38 +0900

    feat : 기본 설정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,10 +1,8 @@
 insert into company(name,country,region)
-values ('원티드','한국','서울');
+values ('원티드','한국','서울'),
+('네이버','한국','판교');
 
 
-insert into employment(position,compensation,content,skill,company_id)
-values ('백엔드 주니어 개발자',1500000,"원티드랩에서 백엔드 주니어 개발자를 '적극' 채용합니다. 자격요건은.",'Python',1),
-('백엔드 주니어 개발자',1500000,"원티드랩에서 백엔드 주니어 개발자를 '적극' 채용합니다. 자격요건은.",'Django',1);
 
-insert into member(id,name,Employment_id)
-values (1,'서명진',1);
+insert into member(name)
+values ('서명진');
